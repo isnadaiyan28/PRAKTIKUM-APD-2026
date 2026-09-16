531d7f8680a5046be110bc880118d7efac6a7e95
ISNA <isna.daiyanfatkiyah28@gmail.com>
2026-09-16 13:57:19 +0800

    mengubah berkas test.py

--- POSTTEST/test.py
+++ POSTTEST/test.py
@@ -1,1 +1,18 @@
-print("hello world")
+nama = 'Isna'
+print(nama)
+
+
+a = "makan"
+b = "nasi"
+print(a + b)
+
+print("makan", "nasi")
+
+teks = "Rahmat Flowchart"
+print(teks[2:3:3])
+
+matakuliah1 = ["APD","Logika Matematika","Kalkulus"]
+print(matakuliah1[1])
+
+nama = input ("masukkan nama anda : ")
+print("Halo, " + nama)
