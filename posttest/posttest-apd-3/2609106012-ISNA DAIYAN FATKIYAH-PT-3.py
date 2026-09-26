e96d3e945593d82f10c172523b48e8fd3c8eab09
ISNA <isna.daiyanfatkiyah28@gmail.com>
2026-09-26 17:06:16 +0800

    menambah perubahan pada 2609106012-ISNA DAIYAN FATKIYAH-PT-3.py

--- posttest/posttest-apd-3/2609106012-ISNA DAIYAN FATKIYAH-PT-3.py
+++ posttest/posttest-apd-3/2609106012-ISNA DAIYAN FATKIYAH-PT-3.py
@@ -2,24 +2,20 @@ print("=" * 40)
 print("     selamat datang di bioskop kami")
 print("=" * 40)
 
-#inisialisasi variabel
 nama_pembeli = input("Silahkan masukkan nama anda: ")
 umur_pembeli = int(input("Silahkan masukkan umur anda: "))
 
-#cek umur pembeli
 if umur_pembeli < 13:
     print("Mohon maaf, anda belum cukup umur untuk menonton")
 else:
     print("Anda cukup umur untuk dapat menonoton, silahkan pilih jenis tiket yang tersedia")
 
-    #jenis tiket
     print("-----pilihan jenis tiket-----")
     print("1. Reguler : Rp50.000")
     print("2. Premium : Rp75.000")
     print("3. VIP     : Rp100.000")
     jenis_tiket = input("masukkan jenis tiket (Reguler/Premium/VIP): ").lower()
-
-    #penentuan harga tiket    
+  
     if jenis_tiket == "reguler" or jenis_tiket == "1":
         harga_tiket = 50000
         jenis_tiket = "Reguler"
@@ -32,25 +28,21 @@ else:
     else:
         harga_tiket = 0
 
-    #pengecekan harga tiket
     if harga_tiket == 0:
         print("jenis tiket yang anda masukkan tidak tersedia, transaksi dibatalkan")
     else:
+
         status_member = input("Apakah anda adalah member (ya/tidak): ").lower()
 
-        #diskon member
         diskon = harga_tiket * 0.2 if status_member == "ya" else 0
         total_bayar_sementara = harga_tiket - diskon
 
-        #biaya admin
         biaya_admin = 0 if status_member == "ya" else 2000   
         total_bayar = total_bayar_sementara + biaya_admin 
-        print(total_bayar)  
+        print("\n" + "total yang harus anda bayarkan adalah : Rp", total_bayar)  
 
-        #bayar total biaya
         bayar = int(input("masukkan jumlah uang yang dibayarkan: Rp"))
         if bayar < total_bayar:
-            kekurangan = total_bayar - bayar
             print("uang yang anda bayarkan kurang, transaksi dibatalkan")
         else:
             if bayar > total_bayar:
@@ -58,7 +50,6 @@ else:
             else:
                 kembalian = 0
 
-            #print struk pembelian
             print("\n" + "=" * 40)
             print("            STRUK PEMBELIAN")
             print("=" * 40)
@@ -68,17 +59,10 @@ else:
             print("Status Member         : ", status_member)
             print("-" * 40)
             print("Harga tiket           : Rp", harga_tiket)
-            if status_member == "ya":
-                print("Diskon member         : Rp", diskon)
-                print("Biaya admin           : Rp0")
-            else:
-                print("Diskon member         : Rp0")
-                print("Biaya admin           : Rp", biaya_admin)
+            print("Diskon member         : Rp", diskon)
+            print("Biaya admin           : Rp", biaya_admin)
             print("-" * 40)
             print("Total bayar           : Rp", total_bayar)
             print("Uang bayar            : RP", bayar )
-            if kembalian > 0:
-                print("Kembalian             : Rp", kembalian)
-            else:
-                print("Kembalian             : Rp0")
+            print("Kembalian             : Rp", kembalian)
             print("=" * 40)
